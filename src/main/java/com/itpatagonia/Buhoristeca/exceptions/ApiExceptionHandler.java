b2d1cd4b576e9ecdd70002775e6cb24bde0800bf
Ignacio Oromendia <ioromendia@dc.uba.ar>
2025-07-08 12:16:28 -0300

    Exception Handler

---
+++ src/main/java/com/itpatagonia/Buhoristeca/exceptions/ApiExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.itpatagonia.Buhoristeca.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return new ResponseEntity<>("Ocurrió un error inesperado", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
