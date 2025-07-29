05a2f1ed130edb1de5a49969f2c325e1735e3c6c
Ignacio Oromendia <ioromendia@dc.uba.ar>
2025-07-29 11:26:38 -0300

    PDF Book Controller Swagger Documentation

--- src/main/java/com/itpatagonia/Buhoristeca/controllers/PDFBookController.java
+++ src/main/java/com/itpatagonia/Buhoristeca/controllers/PDFBookController.java
@@ -1,8 +1,17 @@
 package com.itpatagonia.Buhoristeca.controllers;
 
 import com.itpatagonia.Buhoristeca.dto.BookDto;
+import com.itpatagonia.Buhoristeca.dto.ClientDto;
 import com.itpatagonia.Buhoristeca.dto.PDFBookDto;
+import com.itpatagonia.Buhoristeca.entities.Book;
+import com.itpatagonia.Buhoristeca.exceptions.BookNotFoundException;
+import com.itpatagonia.Buhoristeca.exceptions.ClientNotFoundException;
 import com.itpatagonia.Buhoristeca.services.PDFBookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +24,28 @@ public class PDFBookController {
     @Autowired
     private PDFBookService pdfBookService;
 
+    @Tag(name = "PDF Book")
+    @Operation(summary = "Upload a pdf book")
+    @ApiResponse(
+            description = "PDF uploaded",
+            responseCode = "200",
+            content = {
+                    @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = BookDto.class)
+                    )
+            }
+    )
+    @ApiResponse(
+            description = "Book Not Found",
+            responseCode = "404",
+            content = {
+                    @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = BookNotFoundException.class)
+                    )
+            }
+    )
     @PostMapping("/upload")
     public ResponseEntity<BookDto> uploadPDFBook(
             @RequestParam Integer idBook,
@@ -23,6 +54,28 @@ public class PDFBookController {
         return ResponseEntity.ok(pdfBookService.uploadPDFBook(idBook, file));
     }
 
+    @Tag(name = "PDF Book")
+    @Operation(summary = "Download a pdf book")
+    @ApiResponse(
+            description = "PDF downloaded",
+            responseCode = "200",
+            content = {
+                    @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = byte.class)
+                    )
+            }
+    )
+    @ApiResponse(
+            description = "Not Found",
+            responseCode = "404",
+            content = {
+                    @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ClientNotFoundException.class)
+                    )
+            }
+    )
     @GetMapping("/download")
     public ResponseEntity<byte[]> downloadPDFBook (
             @RequestParam Integer idBook,
