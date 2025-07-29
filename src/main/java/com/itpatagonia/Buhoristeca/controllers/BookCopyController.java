f700c6cc7adf267f7f1b22023af4ad77ac6188d2
Ignacio Oromendia <ioromendia@dc.uba.ar>
2025-07-29 11:17:12 -0300

    Book Copy Controller Swagger Documentation

--- src/main/java/com/itpatagonia/Buhoristeca/controllers/BookCopyController.java
+++ src/main/java/com/itpatagonia/Buhoristeca/controllers/BookCopyController.java
@@ -1,7 +1,15 @@
 package com.itpatagonia.Buhoristeca.controllers;
 
 import com.itpatagonia.Buhoristeca.dto.BookCopyDto;
+import com.itpatagonia.Buhoristeca.dto.BookDto;
+import com.itpatagonia.Buhoristeca.exceptions.BookNotFoundException;
+import com.itpatagonia.Buhoristeca.exceptions.RoleNotFoundException;
 import com.itpatagonia.Buhoristeca.services.BookCopyService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +21,18 @@ public class BookCopyController {
     @Autowired
     private BookCopyService bookCopyService;
 
+    @Tag(name = "Book Copies")
+    @Operation(summary = "Register a new book copy")
+    @ApiResponse(
+            description = "Book copy registered",
+            responseCode = "200",
+            content = {
+                    @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = BookCopyDto.class)
+                    )
+            }
+    )
     @PostMapping("/new")
     public ResponseEntity<BookCopyDto> registerNewBookCopy(
             @RequestParam Integer idBook
@@ -20,6 +40,28 @@ public class BookCopyController {
         return ResponseEntity.ok(bookCopyService.registerNewBookCopies(idBook));
     }
 
+    @Tag(name = "Book Copies")
+    @Operation(summary = "Update book copy state")
+    @ApiResponse(
+            description = "Book copy state updated",
+            responseCode = "200",
+            content = {
+                    @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = BookCopyDto.class)
+                    )
+            }
+    )
+    @ApiResponse(
+            description = "Not Found",
+            responseCode = "404",
+            content = {
+                    @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = BookNotFoundException.class)
+                    )
+            }
+    )
     @PutMapping("/update")
     public ResponseEntity<BookCopyDto> updateBookCopyState(
             @RequestParam Integer idBook,
